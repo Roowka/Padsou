06a0adaff3fcec61a279d1c4625816d7333226e6
Logan <logan.metral04@gmail.com>
2023-01-16 20:44:14 +0100

    fix : fontion dans utils sont maintenant statique

--- app/src/main/java/com/example/padsou/classes/Utils.kt
+++ app/src/main/java/com/example/padsou/classes/Utils.kt
@@ -5,14 +5,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 class Utils {
-    @Composable
-    fun PxToDp(pixels : Float) : Float{
-        val screenPixelDensity = getContext().resources.displayMetrics.density
-        return pixels / screenPixelDensity
-    }
+    companion object Factory {
+        @Composable
+        fun PxToDp(pixels: Float): Float {
+            val screenPixelDensity = getContext().resources.displayMetrics.density
+            return pixels / screenPixelDensity
+        }
 
-    @Composable
-    fun getContext(): Context {
-        return LocalContext.current
+        @Composable
+        fun getContext(): Context {
+            return LocalContext.current
+        }
     }
 }
