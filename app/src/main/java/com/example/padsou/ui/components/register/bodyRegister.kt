1f35a46b3e639542d3b7963f36eccac190be3716
digital <digital@CCIDigital.local>
2023-01-17 11:19:28 +0100

    build: creation du header et du body

---
+++ app/src/main/java/com/example/padsou/ui/components/register/bodyRegister.kt
@@ -0,0 +1,22 @@
+package com.example.padsou.ui.components.register
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.padsou.ui.theme.PadsouTheme
+
+
+@Composable
+fun bodyRegister(){
+
+}
+
+@Preview(
+    showBackground = true
+)
+
+@Composable
+fun bidyRegisterPreview(){
+    PadsouTheme {
+        bodyRegister()
+    }
+}
