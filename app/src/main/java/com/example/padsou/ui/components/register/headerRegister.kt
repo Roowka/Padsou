2aec1f823c5603c4227bd108244966ae531f91db
Aurelien <94925802+Aurelien-6501@users.noreply.github.com>
2023-01-17 11:37:09 +0100

    build: creation du header et du body

--- app/src/main/java/com/example/padsou/ui/components/register/headerRegister.kt
+++ app/src/main/java/com/example/padsou/ui/components/register/headerRegister.kt
@@ -1,2 +1,42 @@
 package com.example.padsou.ui.components.register
 
+import android.icu.text.Transliterator
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.padsou.R
+import com.example.padsou.ui.theme.BlackTitle
+import com.example.padsou.ui.theme.PadsouTheme
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun headerRegister(colorText : Color = BlackTitle ){
+    Column( modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Bienvenue \uD83D\uDE0E", color = colorText, fontWeight = FontWeight.Bold, fontFamily = FontFamily(Font(R.font.integralcf_regular)))
+
+        Text(text = "Inscris-toi pour avoir les meilleurs plans étudiants !", color = colorText, fontFamily = FontFamily(Font(R.font.integralcf_regular)), textAlign = TextAlign.Center, modifier =  Modifier.width(200.dp))
+    }
+
+}
+
+@Preview(
+    showBackground = true
+)
+
+@Composable
+fun headerRegisterPreview(){
+    PadsouTheme {
+        headerRegister()
+    }
+}
