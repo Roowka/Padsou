d9f5202020f03b439c4a11a153ca90a81fe7d6fd
Aurelien-6501 <aurelien.fillion.pro@gmail.com>
2023-01-23 10:58:33 +0100

    build: creation des dossiers et fichiers de la page AddPlan

---
+++ app/src/main/java/com/example/padsou/ui/components/addPlan/bodyAddPlan.kt
@@ -0,0 +1,23 @@
+package com.example.padsou.ui.components.addPlan
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.padsou.ui.components.login.headerLogin
+import com.example.padsou.ui.theme.PadsouTheme
+
+
+@Composable
+fun bodyAddPlan(){
+
+}
+
+
+@Preview(showBackground = true)
+
+
+@Composable
+fun headerLoginPreview() {
+    PadsouTheme {
+        (bodyAddPlan())
+    }
+}
