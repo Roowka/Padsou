2c5c7f72286defdf55ac0fd390e443a0c5870cbf
Logan METRAL <Lo.METRAL@formation-cci.fr>
2023-01-23 14:54:29 +0100

    fix : différent fix sur la page home et login

--- app/src/main/java/com/example/padsou/ui/components/home/HomeHeader.kt
+++ app/src/main/java/com/example/padsou/ui/components/home/HomeHeader.kt
@@ -5,13 +5,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -19,14 +27,19 @@ import com.example.padsou.R
 import com.example.padsou.ui.components.globals.Input_global
 import com.example.padsou.ui.theme.PadsouTheme
 import com.example.padsou.ui.theme.Purple1
+import com.example.padsou.ui.theme.WhiteText
 
 @Composable
-fun HomeHeader(){
-    Column (
+fun HomeHeader() {
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-    ){
-        Column(modifier = Modifier.padding(horizontal = 33.dp).padding(top = 58.dp)) {
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 33.dp)
+                .padding(top = 58.dp)
+        ) {
             Text(
                 text = "Coucou toi,",
                 color = Color.White,
@@ -34,7 +47,8 @@ fun HomeHeader(){
                 fontSize = 26.sp
             )
 
-            Text(text = "T'es en manque de thunes ?",
+            Text(
+                text = "T'es en manque de thunes ?",
                 color = Color.White,
                 fontFamily = FontFamily(Font(R.font.inter_regular)),
                 fontSize = 16.sp,
@@ -43,15 +57,52 @@ fun HomeHeader(){
             )
         }
 
-        Row(modifier = Modifier.padding(horizontal = 33.dp).padding(top = 45.dp, bottom = 34.dp)) {
-            Input_global(inputText = "Cherche un bon plan")
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 33.dp)
+                .padding(top = 45.dp, bottom = 34.dp)
+        ) {
+            var text by remember { mutableStateOf(TextFieldValue("")) }
+            TextField(
+                value = text,
+                onValueChange = { newText ->
+                    text = newText
+                },
+                leadingIcon = {
+                    Icon(
+                        Icons.Filled.Search,
+                        contentDescription = null,
+                        tint = WhiteText
+                    )
+                },
+                shape = RoundedCornerShape(15.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Transparent),
+                textStyle = TextStyle(
+                    fontFamily = FontFamily(Font(R.font.inter_regular))
+                ),
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent
+                ),
+                placeholder = {
+                    Text(
+                        text = "Cherche un bon plan",
+                        color = WhiteText,
+                        fontFamily = FontFamily(Font(R.font.inter_regular))
+                    )
+                }
+            )
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun HomeHeaderPreview(){
+fun HomeHeaderPreview() {
     PadsouTheme {
         HomeHeader()
     }
--- app/src/main/java/com/example/padsou/ui/components/login/bodyLogin.kt
+++ app/src/main/java/com/example/padsou/ui/components/login/bodyLogin.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -19,25 +20,48 @@ import com.example.padsou.ui.theme.GreyText
 import com.example.padsou.ui.theme.PadsouTheme
 
 @Composable
-fun bodyLogin(navController: NavHostController){
-    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Row(modifier = Modifier
-                .width(300.dp)
-                .padding(bottom = 12.dp, top = 12.dp)) {
-                Input_global(inputText = "Ton adresse e-mail")
-            }
-            Row(modifier = Modifier
-                .width(300.dp)
-                .padding(bottom = 12.dp)) {
+fun bodyLogin(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(bottom = 12.dp, top = 12.dp)
+        ) {
+            Input_global(inputText = "Ton adresse e-mail")
+        }
+
+        Column() {
+            Row(
+                modifier = Modifier
+                    .padding(bottom = 17.dp)
+            ) {
                 Input_global(inputText = "Ton mot de passe")
             }
-            Row(modifier = Modifier.padding(bottom = 30.dp, start = 200.dp), horizontalArrangement = Arrangement.End){
-                Text(text = "Mot de passe oublié ?", fontSize = 10.sp, fontWeight = FontWeight.Bold, color = GreyText)
+            Row(
+                modifier = Modifier
+                    .padding(bottom = 30.dp)
+                    .align(Alignment.End),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Text(
+                    text = "Mot de passe oublié ?",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = GreyText,
+                    textAlign = TextAlign.End,
+                    fontFamily = FontFamily(Font(R.font.inter_regular))
+                )
             }
-            Row(modifier = Modifier
-                .width(300.dp)
-                .padding(bottom = 12.dp)) {
-                Btn_global(text = "Se connecter", {navController.navigate("home")})
-            }
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(bottom = 12.dp)
+        ) {
+            Btn_global(text = "Se connecter", { navController.navigate("home") })
+        }
     }
 }
